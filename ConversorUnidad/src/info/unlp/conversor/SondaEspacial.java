7fd2a428f50a569721a2acac81b003ec9b7d6648
Ignacio Nicolas Flores <inifl99@gmail.com>
2020-10-22 19:04:58 -0300

    Update SondaEspacial.java

--- ConversorUnidad/src/info/unlp/conversor/SondaEspacial.java
+++ ConversorUnidad/src/info/unlp/conversor/SondaEspacial.java
@@ -1,43 +1,8 @@
 package info.unlp.conversor;
-import info.unlp.conversor.sistemaInternacional.Area;
-import info.unlp.conversor.sistemaInternacional.KiloGramos;
+
 
 public class SondaEspacial <T> {
-	private double cantidad;
-	private int potencia;
-	Magnitud m;
-	public SondaEspacial(T dato) {
-		//Yo se que T es de tipo magnitud por lo tanto puedo tomarlo como uno
-		switch (((Magnitud)dato).getId()) {
-		case AREA:
-			m=(Area)dato;
-			break;
-		case KILOGRAMOS:
-			m=(KiloGramos)dato;
-		case LIBRA:
-			m=()
-		case METRO:
-		case PIE:
-		case SEGUNDOS:
-		case VELOCIDAD:
-		case SUP:
-		default:
-			break;
-		}
-		
+	public void imprimirMagnitud(T dato) {
+		System.out.println(dato);
 	}
-	public double getCantidad() {
-		return cantidad;
-	}
-	public void setCantidad(double cantidad) {
-		this.cantidad = cantidad;
-	}
-	public int getPotencia() {
-		return potencia;
-	}
-	public void setPotencia(int potencia) {
-		this.potencia = potencia;
-	}
-	
-	
 }
